7068a34037905d5293f1dfa5b31a059aba676857
Victor Prado <e.victorprado@gmail.com>
2024-07-26 17:35:32 -0300

    Declaração de Matrícula funcionando
    Botão 'Declaração de Matrícula' funcionando dentro do esperado. Faltam acertar alguns detalhes na formatação do documento emitido.

--- projeto.py
+++ projeto.py
@@ -1,5 +1,27 @@
 import PySimpleGUI as sg
 import sqlite3
+from fpdf import FPDF
+
+class PDF(FPDF):
+    def header(self):
+        self.image('logo.jpeg', 75, 8, 50)
+        self.ln(1)
+        #self.multi_cell(0, 5, 'UNIVERSIDADE FEDERAL DO ESPÍRITO SANTO', 0, 'C')
+        self.ln(35)
+        self.set_font('Arial', 'B', 18)
+        self.cell(180, 10, 'Declaração de aluno Regular', 0, 1, 'C')
+        # Line break
+        self.ln(10)
+
+    # Page footer
+    def footer(self):
+        # Position at 1.5 cm from bottom
+        self.set_y(-15)
+        # Arial italic 8
+        self.set_font('Arial', 'I', 8)
+        # Page number
+        self.cell(0, 10, 'SIP - Secretaria Integrada de Pós-Graduaçao - CCHN/UFES', 0, 0, 'C')
+
 
 # Conectando ao banco de dados #
 connection = sqlite3.connect('alunos.db')
@@ -102,5 +124,26 @@ while True:
             # Requisitando o banco de dados para atualizar os dados da tabela #
             janela['tabela'].update(values=dados)
 
+    elif event == 'Declaração de Matrícula':
+        if values['tabela'] == []:
+            sg.popup('Favor selecionar um Discente')
+        else:
+            # Preenchendo os inputs com os dados do objeto selecionado # 
+            discente = (dados[values['tabela'][0]])
+            # Instanciando um objeto da classe PDF # 
+            pdf = PDF()
+            pdf.add_page()
+            pdf.set_font('Arial', '', 12)
+            pdf.multi_cell(190, 10, f'Declaro para os fins que se fizerem necessários que {discente[1]}, inscrito no CPF sob o nº {discente[2]}, é aluno regular do Programa de Pós-graduação em {discente[3]} da Universidade Federal do Espírito Santo (UFES) - reconhecido pela CAPES, através da Portaria MEC Nº 609, de 14/03/2019). Publicação no DOU 18 de março de 2019, seç. 1, p.268 - Parecer CNE/CES nº 487/2018.')
+            pdf.ln(10)
+            pdf.multi_cell(190, 10, f'Declaro também que o discente ingressou no semestre {discente[4]} e tem previsão de conclusão em 2 anos.')
+            pdf.ln(60)
+            pdf.cell(180, 10, 'Vitória, ES / 01 de Janeiro de 2024', 0, 0, 'R')
+            
+            pdf.output(f'Declaração de {discente[1]}.pdf', 'F')
+
+            sg.popup('Declaração criada!')
+        
+            
     elif event == sg.WIN_CLOSED:
         break
